ae352503b6ec4ae37f587577c126889eba4476a6
Melisa  Timsina <Meltims@hotmail.com>
2020-04-24 15:53:47 +1000

    Registration html

---
+++ docbook/loginrego/registration.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+ <html>  
+   <head>
+     <title> EHealthCare </title>
+   </head>
+
+   <body>
+        <div>
+        	<form action = "registration.php" method = "post"> 
+        		<div class = "container">
+        		  <h1> EHealthCare Registration</h1>
+
+        		  	<label for= "FName"><b> First Name </b></label>
+        		  	<input type ="text" name="FName" required>
+
+        		  	<label for= "LName"><b> Last Name </b></label>
+        		  	<input type ="text" name="LName" required>
+                     
+                    <label for= "Age"><b> Age </b></label>
+        		  	<input type ="Age" name="LName" required>
+
+        		  	<label for= "EmailAddress"><b> Email Address </b></label>
+        		  	<input type ="Email" name="FName" required>
+
+        		  	<label for= "PNumber"><b> Phone Number </b></label>
+        		  	<input type ="text" name="PNumber" required>
+
+        		  	<label for= "Password"><b> Password </b></label>
+        		  	<input type ="Password" name="Password" required>
+
+
+
+
+
+
+        	</form>
+        
+        </div>
+   </body>
+
+
+
+
+
+
+
+ </html>
