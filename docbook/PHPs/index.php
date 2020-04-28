3aef06d6bce5fe88eb95c0800d7be57d61aacdf8
Vladimir <61957451+VP1996@users.noreply.github.com>
2020-04-28 10:48:35 +1000

    Add files via upload

---
+++ docbook/PHPs/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+ <html>  
+   <head>
+     <title> EHealthCare </title>
+   </head>
+
+   <body>
+        <div>
+        	<form action = "registration.php" method = "post"> 
+        		<div class = "container">
+        		  <h1> EHealthCare Registration</h1>
+
+        		  	<label for= "FName"><b> First Name </b></label>
+        		  	<input type ="text" name="FName" required>
+
+        		  	<label for= "LName"><b> Last Name </b></label>
+        		  	<input type ="text" name="LName" required>
+                     
+                    <label for= "Age"><b> Age </b></label>
+        		  	<input type ="age" name="Age" required>
+
+        		  	<label for= "EmailAddress"><b> Email Address </b></label>
+        		  	<input type ="email" name="Email" required>
+
+        		  	<label for= "PNumber"><b> Phone Number </b></label>
+        		  	<input type ="text" name="PNumber" required>
+
+        		  	<label for= "Password"><b> Password </b></label>
+        		  	<input type ="Password" name="Password" required>
+
+                     <br><br>
+                    <input type="submit" value="Submit" >
+
+
+
+
+        	</form>
+        
+        </div>
+   </body>
+
+
+
+
+
+
+
+ </html>
