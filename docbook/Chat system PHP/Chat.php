e2fb7dbc6a19edd982609a63c3c28f200b4a47b8
kenson <kenson-lang28@hotmail.com>
2020-05-18 22:49:40 +1000

    displays name of messages sent

--- docbook/Chat system PHP/Chat.php
+++ docbook/Chat system PHP/Chat.php
@@ -9,41 +9,105 @@ include 'db.php';
 
 <html>
 
-		<head>
-				<title>Chat</title>
-		</head>
+	<head>
+<title>Chat Box</title> 
 
-<h1> <?php ?>-Online</hl>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="./css/style.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+  <div class="containerchat">
+  
+    <div class="msg-header" style="height:1000px">
+      <div class="msg-header-img" > 	
+       
+    <div class="active">
+    
+
+
+</div>
+<div class="header-icons">
+<center> <i class="fa fa-phone"></i>
+    <i class="fa fa-video-camera"></i>
+    <i class="fa fa-info-circle"></i>
+<center>
+
+
+</div>  
+
+ 
+<!-- This is where the chats ans chat page goes please combine it -->
+
+
+</div>
+
+<div class ="chat-pg">
+    <div class="chats">
+        <div class="msg-pg">
+        <div class="received-msgs">
+        <div class="received-msgs-img">
+   
+</div>
+<br> <br>
+<br> <br>
+<br> <br>
+            <div class="received-msg">
+                <div class="received-msg-inbox">
+                    
+
+        <div class="send-chats">
+            <div class="outgoing-msg">
+                <div class="outgoing-msgs-img">
+                <img src="">
+                </div>
+
+            <div class="outgoing-msg"">
+                <div class="outgoing-msg-inbox">
+                
+           <div>     
+           <div class="outgoing-msgs-img">
+            </div>
+
+         </div>
+		 </div>
+		 </div>
+		 </div>
+		 </div>
+		 
+</body>
+				
+				
 <div class="main">
 
 
-	<div class="output">
+	<div class="output" >
 		<?php $sql = "SELECT * FROM messages ORDER BY date ASC";
 		$result = $conn->query($sql);
 		
-		
 	if ( $result->num_rows > 0 ) {
 		while($row = $result->fetch_assoc()) {
-			echo "" .$row["message"]. " :". $row["date"]. "<br>";
+			echo "". $row["name"].  " " .$row["message"]. " :". $row["date"]. "<br>";
 			echo "<br>";
 			
 		}
 		
-		
 		$conn->close();
-		
 	}
 	?>
 	
 		</div>
 
-
-
 <form method="post" action="send.php">
 
-	<textarea name="msg" placeholder="Type here to send messages." class="send"></textarea> <br>
+	<textarea name="msg" style="width:100%" placeholder="Type here to send messages." class="send"></textarea> <br>
 	
 	<input class="sendBtn" type="submit" value="Send" onclick="disableSend()" >
+<br>
+<br>
+<br>
+<br>
 
 <script type="text/javascript">
     function disableSend(){
@@ -60,6 +124,7 @@ include 'db.php';
 
 
 
+</div> 
 
 
 
