4227e7545299e5c4a39b14a7b9a358d76c6d0144
Kelvin <13228186@student.uts.edu.au>
2020-05-27 03:29:09 +1000

    g

---
+++ docbookPHP/js/FinishedBooking.php
@@ -0,0 +1,117 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+    <header>
+        <title> DocBook </title>
+        <link rel="stylesheet" type="text/css" href= "./css/style1.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="./css/footer.css"> 
+      
+    </header>
+    </header>
+    <body>
+             
+    <div id="page-container">
+        <div id="content-wrap">
+
+        <div class="headerMenu">
+            <div id="wrapper">
+                <div class="logo">
+                    <a href="PatHomePage.php">
+                    <img src="./img/docbook-logo.png"/>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="navbar" >
+            
+           <a href="PatHomePage.php">Home</a> 
+		<a href="PatientProfile.php">Profile</a> 
+        <a href="availDoc.php">Doctors</a> 
+        <a href="PatientPrescriptions.php">Prescriptions</a> 
+		<a href="chatpage.php">Chat</a> 
+
+            
+            
+        </div>
+
+        <div>
+            <br>
+           
+        </div>
+        <br>
+        <div>
+            <br>
+            <br>
+            
+        </div>
+        <div class="container1">
+            <div class="box1" style="flex: 4; background-color: rgba(255, 255, 255, 0.418); border-radius: 20px; padding: 10px;">
+                <br>
+                <h1 style="color:#000; margin-bottom: 20px;text-align: center;"> Thank you for your booking</h1>
+                <br>
+                <h3 style="text-align: center;">Your appointment is being processed. </h3>
+                <br>
+                <br>
+                <p style="color: #000;text-align: center;font-size:25px;">Back to <a style="font-size:25px" href="PatHomePage.php">homepage</a></p>
+            </div>
+            <div class="box2">
+
+            </div>
+            <div class="box3">
+
+            </div>
+        <div>
+
+
+        </div>
+    </div>
+	
+</div>
+</div>
+<div class="w3hubs-footer">
+   	  
+   	  <div class="w3hubs-icon">
+   	  	<ul>
+   	  		<li><a href="#" target="black"><i class="fa fa-facebook"></i></a></li>
+   	  		<li><a href="#" target="black"><i class="fa fa-twitter"></i></a></li>
+   	  		<li><a href="#" target="black"><i class="fa fa-youtube-play"></i></a></li>
+   	  	</ul>
+   	  </div>
+   	  <div class="w3hubs-nav">
+   	  	<ul>
+   	  		<li><a href="PatHomePage.php">Home</a></li>
+   	  		<li><a href="about.php">About</a></li>
+   	  		<li class="p1"><a href="contactUS.php">Contact</a></li>
+   	  		
+
+   	  	</ul>
+   	  </div>
+   	  <div class="w3hubs-nav1">
+   	  	<ul>
+            <li><a href="#">Privacy</a></li>
+            <li><a href="#">T&C's</a></li>         
+   	  		<li><a href="https://github.com/VP1996/SES1A-TM-5">Our GitHub</a></li>
+   	  		<li><a href="https://trello.com/b/IACoN3uK/docbook-development">Our Trello</a></li>
+   	  		
+   	  		
+   	  		
+   	  	</ul>
+   	  </div>
+
+   	  <div class="w3hubs-nav2">
+   	  	<ul>
+   	  		
+   	  		<li><a href="#" title="To Top"><i class="fa fa-chevron-down"></i></a></li>
+   	  		
+   	  	</ul>
+   	  </div>
+   </div>
+
+   <div class="footer-end">
+   	  <p>Copyright &copy; 2020 DocBook </p>
+   </div>
+</body>
+</html>
+   
---
+++ docbookPHP/js/about.php
@@ -0,0 +1,119 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html lang="en">
+    
+    <head>
+    <title>About</title>
+   
+    
+    <link rel="stylesheet" type="text/css" href= "./css/style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="./css/footer.css">
+    <script src="./js/Slideshow.js" type="text/javascript"></script>
+    <body></body>
+
+    <div id="page-container">
+        <div id="content-wrap">
+    
+        <div class="headerMenu">
+            <div class="container2">
+                <div class="container2-box" style="flex-basis: 50%;">
+                    <div id="wrapper">
+                        <div class="logo">
+                            <a href="index.php">
+                            <img src="./img/docbook-logo.png"/>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="container2-box" style="flex-basis: 50%;">
+                    <div style="text-align: right; padding-right: 10px;">
+                        <br>
+                        <br>
+                        <br>
+                        <a href="login.php"><button class="loginbutton">Login</button></a>
+                        <a href="register.php"><button class="loginbutton">Register</button></a>
+                    </div>  
+                </div>
+            </div>
+        </div>
+
+        <div class="navbar">
+            <a href="index.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="contactUS.php">Contact</a>
+        </div>
+        <br>
+        
+        <div class="container1">
+            <div class="box1" style="flex: 3; background-color: rgba(196, 206, 214, 0.822); padding: 10px; border-radius: 8px;" >
+                <h1 style="text-align: center;">Frequently Asked Questions</h1>
+                <br>
+                <h3>1) Question</h3>
+                <p class="normal" style="font-size: 20px;">text</p>
+                <br>
+                <h3>2) Question</h3>
+                <p class="normal" style="font-size: 20px;">text</p>
+                <br>
+                <h3>3) Question</h3>
+                <p class="normal" style="font-size: 20px;">text</p>
+                <br>
+                <h3>4) Question</h3>
+                <p class="normal" style="font-size: 20px;">text</p>
+                <br>
+            </div>
+            <div class="box2">
+
+            </div>
+            <div class="box3">
+
+            </div>
+            </div>
+            </div>
+            </div>
+     
+            <div class="w3hubs-footer">
+   	  
+   	  <div class="w3hubs-icon">
+   	  	<ul>
+   	  		<li><a href="#" target="black"><i class="fa fa-facebook"></i></a></li>
+   	  		<li><a href="#" target="black"><i class="fa fa-twitter"></i></a></li>
+   	  		<li><a href="#" target="black"><i class="fa fa-youtube-play"></i></a></li>
+   	  	</ul>
+   	  </div>
+   	  <div class="w3hubs-nav">
+   	  	<ul>
+   	  		<li><a href="index.php">Home</a></li>
+   	  		<li><a href="about.php">About</a></li>
+   	  		<li class="p1"><a href="contactUS.php">Contact</a></li>
+   	  		
+
+   	  	</ul>
+   	  </div>
+   	  <div class="w3hubs-nav1">
+   	  	<ul>
+            <li><a href="#">Privacy</a></li>
+            <li><a href="#">T&C's</a></li>         
+   	  		<li><a href="https://github.com/VP1996/SES1A-TM-5">Our GitHub</a></li>
+   	  		<li><a href="https://trello.com/b/IACoN3uK/docbook-development">Our Trello</a></li>
+   	  		
+   	  		
+   	  		
+   	  	</ul>
+   	  </div>
+
+   	  <div class="w3hubs-nav2">
+   	  	<ul>
+   	  		
+   	  		<li><a href="#" title="To Top"><i class="fa fa-chevron-down"></i></a></li>
+   	  		
+   	  	</ul>
+   	  </div>
+   </div>
+
+   <div class="footer-end">
+   	  <p>Copyright &copy; 2020 DocBook </p>
+   </div>
+</body>
+</html>
+   
