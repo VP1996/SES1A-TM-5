c7651d046dca53116f019eb814ca7f839a547d2f
Aung Soe Kyaw <aungsoekyaw2017@gmail.com>
2020-05-27 00:14:42 +1000

    added FAQs

--- docbookPHP/about.php
+++ docbookPHP/about.php
@@ -10,65 +10,103 @@
     <script src="./js/Slideshow.js" type="text/javascript"></script>
     <body></body>
 
-    <div id="page-container">
+    <div id="page-container"  style="background-color: rgba(255,255,255,0.9);">
         <div id="content-wrap">
     
-        <div class="headerMenu">
-            <div class="container2">
-                <div class="container2-box" style="flex-basis: 50%;">
-                    <div id="wrapper">
-                        <div class="logo">
-                            <a href="index.php">
-                            <img src="./img/docbook-logo.png"/>
-                            </a>
+            <div class="headerMenu">
+                <div class="container2">
+                    <div class="container2-box" style="flex-basis: 50%;">
+                        <div id="wrapper">
+                            <div class="logo">
+                                <a href="index.php">
+                                <img src="./img/docbook-logo.png"/>
+                                </a>
+                            </div>
                         </div>
                     </div>
+                    <div class="container2-box" style="flex-basis: 50%;">
+                        <div style="text-align: right; padding-right: 10px;">
+                            <br>
+                            <br>
+                            <br>
+                            <a href="login.php"><button class="loginbutton">Login</button></a>
+                            <a href="register.php"><button class="loginbutton">Register</button></a>
+                        </div>  
+                    </div>
                 </div>
-                <div class="container2-box" style="flex-basis: 50%;">
-                    <div style="text-align: right; padding-right: 10px;">
-                        <br>
-                        <br>
-                        <br>
-                        <a href="login.php"><button class="loginbutton">Login</button></a>
-                        <a href="register.php"><button class="loginbutton">Register</button></a>
-                    </div>  
+            </div>
+
+            <div class="navbar">
+                <a href="index.php">Home</a>
+                <a href="about.php">About</a>
+                <a href="contactUS.php">Contact</a>
+            </div>
+            
+            
+            <div class="container1">
+                <div class="box1" style="flex: 3; padding: 10px;" >
+                    <h1 style="text-align: left">Frequently Asked Questions</h1>
+                    <br>
+                    <h3 style="text-align: left">What is docbook?</h3>
+                    <p class="normal" style="font-size: 20px; text-align: justify;">
+                    Docbook is an online service that provides patients with access to doctors via online appointments. 
+                    We provide an online system for you to see a doctor via chat at a time that suits you throung your pc or mobile devices.
+                    With our services, GP will be able to deliver the care in most efficient and fastest way.
+                    <br>
+                    Our aim is towards time-poor patient and those who are not able to access regular health services.
+                    </p>
+                    <br>
+                    <h3 style="text-align: left">How do I book?</h3>
+                    <p class="normal" style="font-size: 20px; text-align: justify;">
+                    Simply visit our website and register an account. If you are already registered, please log-in to our website. <br>
+                    After you have successfully logged in, please follow each steps below for booking.<br>
+                    1) click the "Doctor" button on the navigation bar.<br>
+                    2) choose the doctor on the available list and click the "appointment" button of the preferred doctor.<br>
+                    3) fill the booking form<br>
+                    4) submit<br>
+                    And you are ready for the appointment!
+                    </p>
+                    <br>
+                    <h3 style="text-align: left">What are the benefits?</h3>
+                    <p class="normal" style="font-size: 20px; text-align: justify;">
+                    the main benefit is the convenience of the patients. 
+                    In most cases, transportation could be troublesome if the patient is living alone or there is no easy trasportation.
+                    Using the docbook will not cause any problem thus you can access the health service through the internet.
+                    This is also very useful for patients who do not have enough time for regular health services. 
+                    Most of the consultations take only a few minutes so using docbook is very efficient.
+                     The prescriptions will be available within the appointment day so it is much more easier using the docbook.
+                    </p>
+                    <br>
+                    <h3 style="text-align: left">What if the doctor is not available?</h3>
+                    <p class="normal" style="font-size: 20px; text-align: justify;">
+                    If the doctor is temporarily unavailable for any reason, they will let you know how long they will be. 
+                    You can then choose to wait or cancel the appointment and book another appointment. 
+                    Your consultation does not start until both parties have joined the session.
+                    <br>
+                     
+                    </p>
+                    <br>
+                    <h3 style="text-align: left">What if I need a longer appointment?</h3>
+                    <p class="normal" style="font-size: 20px; text-align: justify;">
+                    The doctor will suggest booking a follow up appointment, if you need more time with the doctor.
+                    </p>
+                    <br>
+                    <h3 style="text-align: left">What if I need to change the appointment?</h3>
+                    <p class="normal" style="font-size: 20px; text-align: justify;">
+                    You can email to us for the changing the appointment date.
+                    </p>
+                    <br>
+                </div>
+                <div class="box2">
+
+                </div>
+                <div class="box3">
+                
                 </div>
             </div>
+            <p style="text-align: center">Still have more questions? <a href="contactUS.php">Contact</a> us!</p>
         </div>
-
-        <div class="navbar">
-            <a href="index.php">Home</a>
-            <a href="about.php">About</a>
-            <a href="contactUS.php">Contact</a>
-        </div>
-        <br>
-        
-        <div class="container1">
-            <div class="box1" style="flex: 3; background-color: rgba(196, 206, 214, 0.822); padding: 10px; border-radius: 8px;" >
-                <h1 style="text-align: center;">Frequently Asked Questions</h1>
-                <br>
-                <h3>1) Question</h3>
-                <p class="normal" style="font-size: 20px;">text</p>
-                <br>
-                <h3>2) Question</h3>
-                <p class="normal" style="font-size: 20px;">text</p>
-                <br>
-                <h3>3) Question</h3>
-                <p class="normal" style="font-size: 20px;">text</p>
-                <br>
-                <h3>4) Question</h3>
-                <p class="normal" style="font-size: 20px;">text</p>
-                <br>
-            </div>
-            <div class="box2">
-
-            </div>
-            <div class="box3">
-
-            </div>
-            </div>
-            </div>
-            </div>
+    </div>
      
     <div class="footer">
         <div>
@@ -81,10 +119,10 @@
                   <th>Contact Us</th>
                 </tr>
                 <tr>
-                  <td><a href="#">Email</a></td>
+                  <td><a href="contactUS.php">Email</a></td>
                 </tr>
                 <tr>
-                  <td><a href="#">Phone</a></td>
+                  <td><a href="contactUS.php">Phone</a></td>
                 </tr>
               </table>
         </div>
