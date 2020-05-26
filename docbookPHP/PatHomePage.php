e7336921c5d9b045d5d721dce41200aa8cafc995
Aung Soe Kyaw <aungsoekyaw2017@gmail.com>
2020-05-27 01:31:43 +1000

    patient home page minor changes

--- docbookPHP/PatHomePage.php
+++ docbookPHP/PatHomePage.php
@@ -3,7 +3,8 @@
 <html>
     <header>
         <title> DocBook </title>
-        <link rel="stylesheet" type="text/css" href= "./css/style.css" />
+        <link rel="stylesheet" type="text/css" href= "./css/style.css" />        
+        <link rel="stylesheet" type="text/css" href= "./css/docleftbar.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans:wght@400;700&display=swap" rel="stylesheet">  
     </header>
@@ -49,6 +50,22 @@
 
         <div class="container1" style="flex-direction: row; padding: 10px;  ">
             <div style="flex-basis: 25%; background-color: rgba(255, 255, 255, 0.774); padding: 1%; overflow: auto;">
+
+            
+            <div class="calendar_header">
+                        <img src="./img/male.png" alt="avatar" style="float:right;border-radius:50%; width:40px; border-style:solid; border-width:thin">
+                        <h1 style="font-size: 22px;" >Welcome Back</h1>
+                        <p class="header_copy"> Schedule</p>
+                      </div>
+                      <div class="calendar_plan">
+                        <div class="cl_plan">
+                        
+                        <div class="cl_title;">Today</div>
+                        <div class="cl_copy">28th  May  2020</div>
+                        <a href="#"> <img src="./img/adddate.png" alt="add" style="float:right;border-radius:50%; width:40px; margin-top:5% "></a>
+                        </div>
+                          </div>
+
                  <h3>Bookings</h3>
                  <hr style="color: #000;">
 
@@ -104,46 +121,34 @@
                     <br>
                     <br>
                 </div>
-            </div>
-        </div>
-
-        <div class="container1">
-            <div class="box1" style="flex: 70%; background-color: rgba(255, 255, 255, 0.767); border-radius: 8px;">
-                <br>
+            
                 <div class="container2">
-                    <div class="container2-box" style="height: 100%;"><img src="./img/appointment1.png" style="vertical-align: middle; align-self: center; padding-left: 50px; padding-right: 50px;"/>
-                        <div>
-                            <br>
-                            <p style="text-align: center;"><b class="big">Step 1. Login or Register</b></p> 
-                            <p class="normal" style="text-align: center;">You will have to login first to make an appointment.<br>Don't have an account?<br>Register <a href="#">Now!</a></p>
-                        </div>
-                    </div>
                     <div class="container2-box" style="height: 100%;"><img src="./img/appointment2.png" style="vertical-align: middle; padding-left: 50px; padding-right: 50px;"/>
                         <div>
                             <br>
-                            <p style="text-align: center;"><b class="big">Step 2. Select an appointment date</b></p>
+                            <p style="text-align: center;"><b class="big">Select an appointment date</b></p>
                             <p class="normal" style="text-align: center;">Create an appointment by selecting the suitable date for you.<br>It can be anytime according to doctors' availability.<br>You can choose to have an appointment now or later for the available time.</p>
                         </div>
                     </div>
                     <div class="container2-box" style="height: 100%;"><img src="./img/appointment3.png" style="vertical-align: middle; padding-left: 50px; padding-right: 50px;"/>
                         <div>
                             <br>
-                            <p style="text-align: center;"><b class="big">Step 3. Confirm</b></p> 
+                            <p style="text-align: center;"><b class="big">Confirm</b></p> 
                             <p class="normal" style="text-align: center;">Complete your appointment by confirming your availability, preferred doctor and medical history.<br>Then proceed to billing and receive your confirmation email.</p>
                         </div>
                     </div>
                     <div class="container2-box" style="height: 100%;"><img src="./img/appointment4.png" style="vertical-align: middle; padding-left: 50px; padding-right: 50px;"/>
                         <div>
                             <br>
-                            <p style="text-align: center;"><b class="big">Step 4. Meet with your doctor online.</b></p> 
+                            <p style="text-align: center;"><b class="big">Meet with your doctor online.</b></p> 
                             <p class="normal" style="text-align: center;">Meet ours doctors through online video call system using your available devices to dicuss.<br>Medical certificate and drug prescription may be provided afterwards</p>
                         </div>
                     </div>
                 </div>
+
             </div>
-            <div class="box2" style="flex-basis: 30%;"></div>
-            <div class="box3" style="flex-basis: 30%;"></div>
         </div>
+
         <br>
 
         <div class="footer" style="position: relative;">
